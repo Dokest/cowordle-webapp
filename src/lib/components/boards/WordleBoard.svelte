532e08298dc7eb3d2fd474ea9418a009a051c11c
Dokest <jorge.moreno.fernandez@outlook.com>
2022-11-19 13:28:00 +0100

    chore: remove some commented code

--- src/lib/components/boards/WordleBoard.svelte
+++ src/lib/components/boards/WordleBoard.svelte
@@ -7,46 +7,6 @@
 	export let showOnlyColors: boolean;
 
 	export let tries: (string | null)[];
-
-	// let currentTry = 0;
-
-	// onMount(() => {
-	// 	const body = document.getElementsByTagName('body')[0];
-
-	// 	body.addEventListener('keypress', (event) => {
-	// 		if (event.code === 'Enter') {
-	// 			return;
-	// 		}
-
-	// 		const currentWord = tries[currentTry];
-
-	// 		if (currentWord === null) {
-	// 			tries[currentTry] = event.key;
-	// 		} else {
-	// 			if (currentWord.length === WORD_LENGTH) {
-	// 				return;
-	// 			}
-
-	// 			tries[currentTry] += event.key;
-	// 		}
-
-	// 		tries = tries;
-	// 	});
-
-	// 	body.addEventListener('keydown', (event) => {
-	// 		if (event.code === 'Backspace') {
-	// 			const currentWord = tries[currentTry];
-
-	// 			if (currentWord === null) {
-	// 				return;
-	// 			}
-
-	// 			tries[currentTry] = currentWord.substring(0, currentWord.length - 1);
-
-	// 			tries = tries;
-	// 		}
-	// 	});
-	// });
 </script>
 
 <div class="mt-5 flex flex-col items-center gap-2">

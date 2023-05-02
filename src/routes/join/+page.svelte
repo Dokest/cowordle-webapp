11c262864e8bd05dac25782096a2b9bafaa865b6
Jorge Moreno <35165785+Dokest@users.noreply.github.com>
2023-05-02 17:19:58 +0200

    Feature(Join): add join page (#6)

---
+++ src/routes/join/+page.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Word from '$lib/components/prefrabs/Word.svelte';
+
+	let code: string = '';
+	let isCodeComplete = true;
+
+	$: {
+		code = formatCode(code);
+
+		isCodeComplete = code.length === 6;
+	}
+
+	function formatCode(newCode: string): string {
+		return newCode.substring(0, 6);
+	}
+
+	function goToRoom(): void {
+		goto(`/room/${code}`);
+	}
+</script>
+
+<div class="mt-5 w-[90%] md:w-[50%] mx-auto flex flex-col items-center gap-3">
+	<h2 class="text-xl font-semibold">JOIN A ROOM</h2>
+	<p>Use the room code given by a player to join their match.</p>
+
+	<div class="">
+		<label for="code_input">
+			<Word bind:word={code} length={6} results={[]} showOnlyColors={false} />
+		</label>
+
+		<input
+			type="text"
+			name="code_input"
+			id="code_input"
+			bind:value={code}
+			class="absolute opacity-0 pointer-events-none scale-0"
+		/>
+	</div>
+
+	<button
+		on:click={goToRoom}
+		disabled={!isCodeComplete}
+		class="px-2 py-1 border-2 rounded-lg font-semibold {isCodeComplete
+			? 'border-green-700 bg-neutral-900'
+			: 'border-neutral-900'}">Join!</button
+	>
+
+	<p class="text-sm">
+		<span class="font-bold text-base">TIP!</span> You can also use the link directly to join the
+		room.
+	</p>
+</div>
